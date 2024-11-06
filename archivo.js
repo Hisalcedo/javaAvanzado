e0fbc6b3da71681733d08f82bbabdefe74289051
humberto ibarra <hisalcedo@hotmail.com>
2024-11-05 21:08:06 -0600

    nuevos cambios

--- archivo.js
+++ archivo.js
@@ -1,2 +1,19 @@
-console.log("hola mundo");
+let allPokemons = []
+const getpokemons = (limit) => {
+const url =`https://pokeapi.co/api/v2/pokemon?limit=${limit}`
+axios.get(url)
+.then(Response ) => {
+    let infopokemons = Response.data.results
+}
+}
 
+let createCards = (pokemons)=>{
+let PokemonsContainer = document.getElementById("PokemonsContainer")
+pokemons.forEach(pokemons) => {
+    let card = document.createElement("div")
+    let title = document.createElement("p")
+    let image = document.createElement("img")
+    card.append(title,image);
+    PokemonsContainer.append(card)
+};
+}
